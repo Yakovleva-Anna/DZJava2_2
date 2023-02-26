8fb0d39a6445152b007eccecb0aae040801fdbfb
Yakovleva Anna <Yakovleva_A@kadis.ru>
2023-02-26 23:01:21 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         int start = 100; // текущий счёт
         int refill = 1100; // сумма пополнения
         int costbonus = 100; // сколько рублей стоит 1 бонус
-        int bonus= refill / costbonus;
+        int bonus = refill / costbonus;
         if (refill > 1000) {
             System.out.println("Ваш балланс " + (start + refill + bonus));
             System.out.println("Количество бонусных рублей " + bonus);
@@ -21,8 +21,8 @@ public class Main {
         int start = 100; // текущий счёт
         int refill = 1100; // сумма пополнения
         int costbonus = 100; // сколько рублей стоит 1 бонус
-        int bonus= refill / costbonus;
-        boolean b = refill>1000;
+        int bonus = refill / costbonus;
+        boolean b = refill > 1000;
         if (b) {
             System.out.println("Ваш балланс " + (start + refill + bonus));
             System.out.println("Количество бонусных рублей " + bonus);
@@ -38,8 +38,8 @@ public class Main {
 
         int start = 100; // текущий счёт
         int refill = 1100; // сумма пополнения
-        int bonus= refill / 100;
-        boolean b = refill>1000;
+        int bonus = refill / 100;
+        boolean b = refill > 1000;
         if (b) {
             System.out.println("Ваш балланс " + (start + refill + bonus));
             System.out.println("Количество бонусных рублей " + bonus);
