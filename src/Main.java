9f82f150cf58c379a93a73bd36443d89c8153046
Yakovleva Anna <Yakovleva_A@kadis.ru>
2023-02-26 22:39:59 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int start = 100; // текущий счёт
+        int refill = 1100; // сумма пополнения
+        int costbonus = 100; // сколько рублей стоит 1 бонус
+        int bonus= refill / costbonus;
+        if (refill > 1000) {
+            System.out.println("Ваш балланс " + (start + refill + bonus));
+            System.out.println("Количество бонусных рублей " + bonus);
+        } else {
+            System.out.println("Ваш балланс " + (start + refill));
+        }
+    }
+}
+
+
+/* public class Main {
+    public static void main(String[] args) {
+
+        int start = 100; // текущий счёт
+        int refill = 1100; // сумма пополнения
+        int costbonus = 100; // сколько рублей стоит 1 бонус
+        int bonus= refill / costbonus;
+        boolean b = refill>1000;
+        if (b) {
+            System.out.println("Ваш балланс " + (start + refill + bonus));
+            System.out.println("Количество бонусных рублей " + bonus);
+        } else {
+            System.out.println("Ваш балланс " + (start + refill));
+        }
+    }
+}
+*/
+
+/* public class Main {
+    public static void main(String[] args) {
+
+        int start = 100; // текущий счёт
+        int refill = 1100; // сумма пополнения
+        int bonus= refill / 100;
+        boolean b = refill>1000;
+        if (b) {
+            System.out.println("Ваш балланс " + (start + refill + bonus));
+            System.out.println("Количество бонусных рублей " + bonus);
+        } else {
+            System.out.println("Ваш балланс " + (start + refill));
+        }
+    }
+}
+*/
